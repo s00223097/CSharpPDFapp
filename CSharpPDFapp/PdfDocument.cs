825c67562a1f3e9eec613dbbc315e5b1f0b103cc
Saniya Mathewkutty <S00223097@atu.ie>
2023-04-19 16:26:26 +0100

    added xaml and code behind them, removed bookmark extractor overusing regex

---
+++ CSharpPDFapp/PdfDocument.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Xps.Packaging;
+using System.Xml;
+
+
+namespace CSharpPDFapp
+{
+    public class PdfDocument
+    {
+        // Declare variables 
+        private string filePath;
+        private XpsDocument xpsDocument;
+        private FixedDocumentSequence documentSequence;
+
+        // constructor
+        public PdfDocument(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public void LoadPdfDocument()
+        {
+            // Load PDF document using XpsDocument class
+            xpsDocument = new XpsDocument(filePath, FileAccess.Read);
+            documentSequence = xpsDocument.GetFixedDocumentSequence();
+        }
+
+        public void DisplayPdfDocument(FlowDocumentReader reader) // flowdocument is in system.windows as a window control option 
+        {
+            // Display PDF document using FlowDocumentReader control 
+            var paginator = documentSequence.DocumentPaginator;
+            var document = new FlowDocument(); 
+            var block = new BlockUIContainer((System.Windows.UIElement)paginator.GetPage(0).Visual); // pick the Window to display the pdf in 
+            document.Blocks.Add(block); // add 
+            reader.Document = document;
+        }
+
+        public void ClosePdfDocument()
+        {
+            // Close PDF document
+            if (xpsDocument != null) // if no document has been loaded...
+            {
+                xpsDocument.Close(); 
+            }
+        }
+
+    }
+}
+/* 
+ 
+ */
---
+++ CSharpPDFapp/PdfReaderWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Win32;
+using PdfReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+namespace CSharpPDFapp
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class PdfReaderWindow : Window
+    {
+        private PdfDocument pdfDocument;
+        private FlowDocumentReader pdfReader;
+
+        public PdfReaderWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenPdfDocument_Click(object sender, RoutedEventArgs e)
+        {
+            // Open PDF document and display in FlowDocumentReader control
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                if (pdfDocument != null)
+                {
+                    pdfDocument.ClosePdfDocument();
+                }
+                pdfDocument = new PdfDocument(openFileDialog.FileName);
+                pdfDocument.LoadPdfDocument();
+                pdfDocument.DisplayPdfDocument(pdfReader);
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            // Close PDF document and exit application
+            if (pdfDocument != null)
+            {
+                pdfDocument.ClosePdfDocument();
+            }
+            Application.Current.Shutdown();
+        }
+    }
+}
+/*
+using Microsoft.Win32;
+
+namespace PdfReader
+{
+    public partial class PdfReaderWindow : Window
+    {
+        private PdfDocument pdfDocument;
+
+        public PdfReaderWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenPdfDocument_Click(object sender, RoutedEventArgs e)
+        {
+            // Open PDF document and display in FlowDocumentReader control
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                if (pdfDocument != null)
+                {
+                    pdfDocument.ClosePdfDocument();
+                }
+                pdfDocument = new PdfDocument(openFileDialog.FileName);
+                pdfDocument.LoadPdfDocument();
+                pdfDocument.DisplayPdfDocument(pdfReader);
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            // Close PDF document and exit application
+            if (pdfDocument != null)
+            {
+                pdfDocument.ClosePdfDocument();
+            }
+            Application.Current.Shutdown();
+        }
+    }
+}
+
+
+
+*/
